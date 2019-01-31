3870298414703a48e19e61c2ddf833bc91ddffd4
grol <grolegor@gmail.com>
2019-02-01 01:22:59 +0300

    complete chapter 2

--- ch02/taco/src/main/kotlin/tacos/Order.kt
+++ ch02/taco/src/main/kotlin/tacos/Order.kt
@@ -7,27 +7,27 @@ import org.hibernate.validator.constraints.CreditCardNumber
 
 data class Order(
 
-    @NotBlank(message = "Name is required")
-    var name: String,
+    @field: NotBlank(message = "Name is required")
+    var name: String? = null,
 
-    @NotBlank(message = "Street is required")
-    var street: String,
+    @field: NotBlank(message = "Street is required")
+    var street: String? = null,
 
-    @NotBlank(message = "City is required")
-    var city: String,
+    @field: NotBlank(message = "City is required")
+    var city: String? = null,
 
-    @NotBlank(message = "State is required")
-    var state: String,
+    @field: NotBlank(message = "State is required")
+    var state: String? = null,
 
-    @NotBlank(message = "Zip code is required")
-    var zip: String,
+    @field: NotBlank(message = "Zip code is required")
+    var zip: String? = null,
 
-    @CreditCardNumber(message = "Not a valid credit card number")
-    var ccNumber: String,
+    @field: CreditCardNumber(message = "Not a valid credit card number")
+    var ccNumber: String? = null,
 
-    @Pattern(regexp = "^(0[1-9]|1[0-2])([/])([1-9][0-9])$", message = "Must be formatted MM/YY")
-    var ccExpiration: String,
+    @field: Pattern(regexp = "^(0[1-9]|1[0-2])([/])([1-9][0-9])$", message = "Must be formatted MM/YY")
+    var ccExpiration: String? = null,
 
-    @Digits(integer = 3, fraction = 0, message = "Invalid CVV")
-    var ccCVV: String
+    @field: Digits(integer = 3, fraction = 0, message = "Invalid CVV")
+    var ccCVV: String? = null
 )
---
+++ ch02/taco/src/main/kotlin/tacos/web/WebConfig.kt
@@ -0,0 +1,14 @@
+package tacos.web
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebConfig : WebMvcConfigurer {
+
+    override fun addViewControllers(registry: ViewControllerRegistry) {
+        registry.addViewController("/").setViewName("home")
+    }
+
+}
